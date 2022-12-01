1b8b8974f9faa9a24a3a6791042ac07ec2d910b5
Tiago Miguel Cunha <tiago92cunha@gmail.com>
2022-12-01 15:03:15 +0000

    Refactor code

--- 01/a.ts
+++ 01/a.ts
@@ -1,30 +1,21 @@
 //First Part
-
 const results = await Deno.readTextFile("input.txt");
-const splited = results.split("\n\n");
-
-
-const array = [];
-let index = 0;
-
+const splitted = results.split("\n\n");
+const array: Array<number>= [];
 let biggest = 0;
 
-console.log(splited);
-
-splited.forEach((element) => {
+splitted.forEach((element) => {
     const result = element.split("\n").map((x) => parseInt(x));
-    let total = result.reduce((a, b) => a + b, 0);
+    const total = result.reduce((a, b) => a + b, 0);
     array.push(total);
 
     if (total > biggest) biggest = total;
 });
-
 console.log("Part A: ", biggest);
+
 // // Second part
 
  array.sort(function(a, b) { return  b - a });
-
-
  console.log("Part B: ",  array[0] + array[1] + array[2]);
 
 
